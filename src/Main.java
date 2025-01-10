9076591f6871c28a22ecd7fd9e4db814da2e8982
pessijoh <johpessi@gmail.com>
2025-01-10 09:59:24 +0200

    lektion 1 delvis 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello java!");
-    }
 
+        Car car = new Car( "Toyota");
+        Bike bike = new Bike("Scott");
+
+        System.out.println(car.getName() + " is " + car.getType());
+        System.out.println(bike.getName() + " is " + bike.getType());
+
+    }
 }
+
