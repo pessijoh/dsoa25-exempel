277eb880664a4b12634c40612ea07665ab705faf
pessijoh <johpessi@gmail.com>
2025-01-16 12:16:32 +0200

    Lektion 3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args){
-        Shirt shirt = new Shirt( "Shirt");
 
-        System.out.println(shirt.getName() + " is " + shirt.getType() + " " + shirt.getColor() + " " + shirt.getPrice() + " " + shirt.getBrand());
 
+        Calculator Calc = new Calculator(); {
+
+        }
     }
 
 }
