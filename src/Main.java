e35b7edfe21560f49b780d7397f9027c84568313
pessijoh <johpessi@gmail.com>
2025-02-04 11:59:39 +0200

    Lektion 9

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,36 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
 
-    // Komstanter med static final
-    static final String PROGRAM_VERSION = "1.0";
-    static final int DAYS_IN_WEEK = 7;
-
     public static void main(String[] args) {
+        String[] sortedData = {
+                "Aragorn", "Arwen", "Bilbo", "Boromir", "Celeborn", "Denethor", "Éomer", "Éowyn",
+                "Faramir", "Galadriel", "Galandriel", "Gimli", "Gandalf", "Glorfindel", "Gollum",
+                "Legolas", "Merry", "Pippin", "Radagast", "Sam", "Saruman", "Théoden", "Tom"};
 
-        Scanner scanner = new Scanner(System.in);
+        Person p1 = new Person(sortedData[0]);
+        Person p2 = new Person(sortedData[1], p1);
+        Person p3 = new Person(sortedData[2], p2);
+        Person p4 = new Person(sortedData[3], p3);
 
-        /*
-        new CCRandomRundown();
-         */
 
-        BikeShop shop = new BikeShop();
-        shop.addBike(BikeFactory.createExpensiveBike("Scott"));
-        shop.addBike(new Bike("Nishiki", 500));
-        shop.addBike(BikeFactory.createCheapBike("Insera"));
-
-        System.out.println("Välkommen till BikeShop! Våra cyklar:");
-        for (int i = 0; i < shop.getBikeCount(); i++) {
-            System.out.printf("%d - %s %.2f € lagersaldo: %s\n",
-                    i,
-                    shop.getBike(i).getName(),
-                    shop.getBike(i).getPrice(),
-                    shop.getBike(i).getStock()
-            );
-
+        Person current = p4;
+        while(current != null) {
+            System.out.println(current.getName());
+            current = current.next;
         }
 
 
-        String userInput;
 
-        while (true) {
+        StringArray names = new StringArray(sortedData);
 
-            System.out.print("Vilken cykel vill du köpa? (q för quit) ");
 
-            userInput = scanner.nextLine();
+        names.add("Gollum");
+        System.out.println(names.toString());
 
-            if (userInput.equalsIgnoreCase("q")) {
-                System.out.println("Tack, välkommen åter!");
-                break;
-            }
+        names.reverse();
+        System.out.println(names.toString());
 
-            if (!Utils.isInteger(userInput)) {
-                System.out.println("Du måste ge ett heltal!");
-                continue;
-            }
 
-            int bikeIndex = Integer.parseInt(userInput);
-            if (bikeIndex >= shop.getBikeCount()) {
-                System.out.println("Det finns ingen sådan cykel");
-                continue;
-            }
-
-            System.out.printf("Grattis, du köpte nu en %s för %.2f\n",
-                    shop.getBike(bikeIndex).getName(),
-                    shop.getBike(bikeIndex).getPrice()
-            );
-            break;
-
-        }
 
         System.exit(0);
-
-
     }
 }
---
+++ src/StringArray.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class StringArray {
+
+    String[] values;
+
+    public StringArray(String[] values) {
+        System.out.println("new stringArray");
+        this.values = values;
+    }
+    public void add(String value){
+        System.out.println("add" + value);
+        String[] newValues = new String[values.length+1];
+        for (int i = 0; i < values.length; i++) {
+            newValues[i] = values[i];
+        }
+        newValues[values.length] = value;
+        values = newValues;
+   }
+    public void reverse() {
+    String[] newValues = new String[values.length];
+        for (int i = 0; i < values.length; i++) {
+            newValues[i] = values[values.length -1 -i];
+
+        }
+        values = newValues;
+    }
+
+
+   public String toString(){
+        return Arrays.toString(values);
+   }
+}
+
